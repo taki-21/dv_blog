e6f609f5f320330092649bf9a3185f8786bd1ef8
saka-21 <kasaikasai572@gmail.com>
2020-07-18 20:22:13 +0900

    Update: componentsの編集

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, pagination, response
 from .models import Post, Category
 from .serializers import CategorySerializer, PostSerializer, SimplePostSerializer
 
+
+class StandardResultSetPagination(pagination.PageNumberPagination):
+    page_size = 10
+
+    def get_paginated_response(self, data):
+        return response.Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'results': data,
+            'page_size': self.page_size,
+            'range_first': (self.page.number * self.page_size) - (self.page_size) + 1,
+            'range_last': min((self.page.number * self.page_size), self.page.paginator.count),
+        })
 
 class CategoryList(generics.ListAPIView):
     queryset = Category.objects.all()
@@ -11,6 +27,7 @@ class CategoryList(generics.ListAPIView):
 class PostList(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = SimplePostSerializer
+    pagination_class = StandardResultSetPagination
 
 
 class PostDetail(generics.RetrieveAPIView):
